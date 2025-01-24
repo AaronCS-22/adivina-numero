ab60244c87d3059f1109ef8ffa950a98459e31a9
AaronCS-22 <centenoa22@gmail.com>
2025-01-24 14:26:29 +0100

    InitData y selección enelemtos

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+const INITIAL_SCORE = 20
+const MAX_NUMBER = 20
+
+initData()
+
+function initData() {
+  let score = INITIAL_SCORE
+  let highscore = 0
+  let numAleat = Math.trunc(Math.random() * MAX_NUMBER) + 1
+}
+
+const messageField = document.querySelector('.message')
+const scoredFile = document.querySelector('.score')
+const highscoreField = document.querySelector('.highscore')
+const numberField = document.querySelector('.number')
+const gessField = document.querySelector('.guess')
+const checkButton = document.querySelector('.check')
+const againButton = document.querySelector('.again')
+
+checkButton.addEventListener('click', checkNumber)
+
+function checkNumber() {
+  // Ver numero introducido
+}
